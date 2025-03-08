2f693ee6093fb0512b0fa10138207e5259401cc9
mirzaibtisam41 <mirzaibtisam41@gmail.com>
2025-03-08 16:12:50 +0500

    change backend variables to env file

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -1,22 +1,22 @@
-const cloudinary = require("cloudinary").v2;
-const multer = require("multer");
+const cloudinary = require('cloudinary').v2;
+const multer = require('multer');
 
 cloudinary.config({
-  cloud_name: "dpfqoeiig",
-  api_key: "515978471981276",
-  api_secret: "wtEqLTHICTm8a4JLkTJye9QtKqE",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new multer.memoryStorage();
 
 async function imageUploadUtil(file) {
   const result = await cloudinary.uploader.upload(file, {
-    resource_type: "auto",
+    resource_type: 'auto',
   });
 
   return result;
 }
 
-const upload = multer({ storage });
+const upload = multer({storage});
 
-module.exports = { upload, imageUploadUtil };
+module.exports = {upload, imageUploadUtil};
