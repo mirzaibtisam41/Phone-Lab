c9840963de05e9d579358d203f573db1424fdca7
mirzaibtisam41 <mirzaibtisam41@gmail.com>
2025-03-08 16:30:21 +0500

    add default route to check for server running or not

--- server/server.js
+++ server/server.js
@@ -44,6 +44,9 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Server Running');
+});
 app.use('/api/auth', authRouter);
 app.use('/api/admin/products', adminProductsRouter);
 app.use('/api/admin/orders', adminOrderRouter);
